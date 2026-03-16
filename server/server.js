bf869496c30389f08f1ce18050ff356e5f8aee24
Sambhav Goel <sambhavgoelyash0907@gmail.com>
2026-03-17 00:44:38 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
+require('dns').setServers(['8.8.8.8']);
+require('dns').setDefaultResultOrder('ipv4first');
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+
+
 
 const app = express();
 
@@ -12,9 +16,14 @@ app.use(express.json());
 // Using the LONG connection format to bypass DNS SRV blocks
 // z3HHbV4JgneKcs0t
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log("✅ Connected to MongoDB Atlas"))
-  .catch(err => console.error("❌", err.message));
+mongoose.connect(process.env.MONGODB_URI, {
+  serverSelectionTimeoutMS: 10000,
+})
+.then(() => console.log("✅ Mongo db is successfully connected"))
+.catch(err => {
+  console.log("FULL ERROR:");
+  console.dir(err, { depth: null });
+});
 
 // --- SCHEMA & MODEL ---
 const transactionSchema = new mongoose.Schema({
@@ -32,7 +41,8 @@ const TransactionModel = mongoose.model('Transaction', transactionSchema);
 // GET All
 app.get('/api/transactions', async (req, res) => {
     try {
-        const transactions = await TransactionModel.find().sort({ _id: -1 });
+        const currentTenant = req.query.tenantId || 'tenant_acme_123'
+        const transactions = await TransactionModel.find({tenantId: currentTenant}).sort({ _id: -1 });
         res.json(transactions); 
     } catch (err) {
         res.status(500).json({ error: err.message });
@@ -42,10 +52,15 @@ app.get('/api/transactions', async (req, res) => {
 // POST New
 app.post('/api/transactions', async (req, res) => {
     try {
-        const newTransaction = new TransactionModel(req.body);
+        const transactionData = {
+            ...req.body,
+            tenantId: req.body.tenantId || 'tenant_acme_123'
+        }
+        const newTransaction = new TransactionModel(transactionData);
         const savedTransaction = await newTransaction.save();
         res.status(201).json(savedTransaction);
     } catch (err) {
+        console.error("Save Error:", err.message);
         res.status(400).json({ error: err.message });
     }
 });
